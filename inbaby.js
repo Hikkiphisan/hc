93d05352760cf66ffbf0275719de7b15d88b834d
Hikkiphisan <locphihuu@gmail.com>
2024-03-08 08:45:43 +0700

    ggg

---
+++ inbaby.js
@@ -0,0 +1,7 @@
+// script1.js
+var a = 3;
+var b = 5;
+var sum = a + b
+console.log("Giá trị của a trong script1.js:", a);
+console.log("Giá trị của b trong script1.js:", b);
+document.write('sum')
---
+++ index.js
@@ -0,0 +1,4 @@
+let a=6;
+let b=5;
+    let sum= a + b;
+    document.write("index "+sum)
